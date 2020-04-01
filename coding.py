5b35215ba36bc04e11ae36bb0091b6176fbade30
junpengxu <weakeexu@gmai.com>
2020-04-01 22:41:04 +0800

    19. 删除链表的倒数第N个节点

--- coding.py
+++ coding.py
@@ -1,30 +1,37 @@
-import itertools
-from typing import List
-
-
-# class Solution:
-#     def letterCombinations(self, digits: str) -> List[str]:
-#         phone = {2: 'abc', 3: 'def', 4: 'ghi', 5: 'jkl', 6: 'mno', 7: 'pqrs', 8: 'tuv', 9: 'wxyz'}
-#         return [''.join(_) for _ in itertools.product(*(phone.get(int(_)) for _ in digits))] if digits else []
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 
 class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        phone = {2: 'abc', 3: 'def', 4: 'ghi', 5: 'jkl', 6: 'mno', 7: 'pqrs', 8: 'tuv', 9: 'wxyz'}
-        if len(digits)==0:
-            return []
-        res = ['']
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        L = ListNode(-1)
+        L.next = head
+        i = 0
+        j = L
+        k = L
+        while j.next:
+            i += 1
+            j = j.next
+            if i - n > 0:
+                k = k.next
+        k.next = k.next.next
+        return L.next
 
-        def dfs(nums, res):
-            result = []
-            if not nums:
-                return res
-            num = nums[0]
-            for i in list(phone[int(num)]):
-                for j in res:
-                    result.append(j+i)
-            return dfs(nums[1:], result)
 
-        a =  dfs(digits, res)
-        return a
-print(Solution().letterCombinations('23'))
+head = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(3)
+node4 = ListNode(4)
+node5 = ListNode(5)
+head.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+if __name__ == '__main__':
+    res = Solution().removeNthFromEnd(head, 5)
+    print(head.val)
+    while head.next:
+        head = head.next
+        print(head.val)
