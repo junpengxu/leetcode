a868322817fc0edc2f74b87d614034772ed23c33
junpengxu <weakeexu@gmai.com>
2020-04-08 21:37:25 +0800

    20. Valid Parentheses.md

--- coding.py
+++ coding.py
@@ -1,37 +1,25 @@
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-
 class Solution:
-    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        L = ListNode(-1)
-        L.next = head
-        i = 0
-        j = L
-        k = L
-        while j.next:
-            i += 1
-            j = j.next
-            if i - n > 0:
-                k = k.next
-        k.next = k.next.next
-        return L.next
+    def isValid(self, s: str) -> bool:
+        parent_dict = {')': '(', '}': '{', ']': '['}
+        tmp = []
+        if len(s) == 1:
+            return False
+        for item in s:
+            if item not in parent_dict:
+                tmp.append(item)
+            elif not tmp:
+                return False
+            elif tmp[-1] != parent_dict[item]:
+                return False
+            else:
+                tmp.pop(-1)
+        return tmp == []
 
 
-head = ListNode(1)
-node2 = ListNode(2)
-node3 = ListNode(3)
-node4 = ListNode(4)
-node5 = ListNode(5)
-head.next = node2
-node2.next = node3
-node3.next = node4
-node4.next = node5
 if __name__ == '__main__':
-    res = Solution().removeNthFromEnd(head, 5)
-    print(head.val)
-    while head.next:
-        head = head.next
-        print(head.val)
+    assert Solution().isValid('()(){}') == True
+    assert Solution().isValid('()(){[}') == False
+    assert Solution().isValid('()') == True
+    assert Solution().isValid('(]') == False
+    assert Solution().isValid('((') == False
+    assert Solution().isValid('){') == False
