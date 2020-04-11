e527953e4cb6d43dc0528dc55831de90c8ed9ccc
junpengxu <weakeexu@gmai.com>
2020-04-11 14:43:45 +0800

    22. Generate Parentheses

--- coding.py
+++ coding.py
@@ -1,25 +1,19 @@
 class Solution:
-    def isValid(self, s: str) -> bool:
-        parent_dict = {')': '(', '}': '{', ']': '['}
-        tmp = []
-        if len(s) == 1:
-            return False
-        for item in s:
-            if item not in parent_dict:
-                tmp.append(item)
-            elif not tmp:
-                return False
-            elif tmp[-1] != parent_dict[item]:
-                return False
-            else:
-                tmp.pop(-1)
-        return tmp == []
+    def generateParenthesis(self, n):
+        res = []
+        self.dfs(res, n, n, '')
+        return res
+
+    def dfs(self, res, left, right, path):
+        if left == 0 and right == 0:
+            res.append(path)
+            return res
+        if left > 0:
+            self.dfs(res, left - 1, right, path + '(')
+        # 左括号比右括号少的情况下才增右括号
+        if right > 0:
+            self.dfs(res, left, right - 1, path + ')')
 
 
 if __name__ == '__main__':
-    assert Solution().isValid('()(){}') == True
-    assert Solution().isValid('()(){[}') == False
-    assert Solution().isValid('()') == True
-    assert Solution().isValid('(]') == False
-    assert Solution().isValid('((') == False
-    assert Solution().isValid('){') == False
+    Solution().generateParenthesis(3)
