75cf04384442e8cf954cdd0022736c13de882f4e
junpengxu <weakeexu@gmai.com>
2020-05-07 21:51:56 +0800

    24. Swap Nodes in Pairs

--- coding.py
+++ coding.py
@@ -1,17 +1,30 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        l3 = ListNode()
-        cur = l3
-        while l1 and l2:
-            if l1.val < l2.val:
-                cur.next = ListNode(l1.val)
-                l1 = l1.next
-            else:
-                cur.next = ListNode(l2.val)
-                l2 = l2.next
-            cur = cur.next
-        if l1:
-            cur.next = l1
-        if l2:
-            cur.next = l2
-        return l3.next
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if not head: return head
+        cur = head
+        while cur.next:
+            p = cur
+            q = cur.next
+            p.val, q.val = q.val, p.val
+            cur = cur.next.next if cur.next.next else cur.next
+        return head
+
+
+if __name__ == '__main__':
+    a1 = ListNode(1)
+    a2 = ListNode(2)
+    a3 = ListNode(3)
+    a4 = ListNode(4)
+    head = a1
+    head.next = a2
+    a2.next = a3
+    a3.next = a4
+    A = Solution()
+    aa = A.swapPairs(head)
+    aa = A.swapPairs(head)
