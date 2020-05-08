76f69c522cd72d6aa1e7802ed3ad63d5d6247788
junpengxu <weakeexu@gmai.com>
2020-05-08 22:41:44 +0800

    28. Implement strStr()

--- coding.py
+++ coding.py
@@ -1,17 +1,12 @@
 class Solution:
-    def removeElement(self, nums, val):
-        flag = 0
-        for i in range(0, len(nums)):
-            if nums[i] != val:
-                nums[flag] = nums[i]
-                flag += 1
-        return nums[:flag + 1]
-
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle not in haystack:
+            return -1
+        return haystack.index(needle)
 
 if __name__ == '__main__':
     A = Solution()
-    nums = [0, 0, 1, 1, 1, 2, 0, 2, 3, 3, 4]
-    aa = A.removeElement(nums, 0)
+    aa = A.strStr('aaaabcd', 'ab')
     print(aa)
 
 
