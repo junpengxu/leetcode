f02dfa5b6b03de37b244c7b5f56ff833fd14893f
junpengxu <weakeexu@gmai.com>
2020-05-12 01:12:07 +0800

    33. Search in Rotated Sorted Array

--- coding.py
+++ coding.py
@@ -1,14 +1,30 @@
 class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        for i in range(len(nums)-1, 0, -1):
-            if nums[i] > nums[i-1]:
-                nums[i:] = sorted(nums[i:])
-                for j in range(i, len(nums)):
-                    if nums[j] > nums[i-1]:
-                        nums[j], nums[i-1] = nums[i-1], nums[j]
-                        break
-                return
-        nums.sort()
+    def search(self, nums, target: int) -> int:
+        if not nums:
+            return -1
+        left = 0
+        right = len(nums) - 1
+        self.aim = -1
+        self.binarysearch(nums, left, right, target)
+        return self.aim
+
+    def binarysearch(self, nums, left, right, target):
+
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            self.aim = mid
+            return
+        elif nums[left] == target:
+            self.aim = left
+            return
+        elif nums[right] == target:
+            self.aim = right
+            return
+        if right - left > 1 and self.aim == -1:
+            self.binarysearch(nums, mid + 1, right, target)
+            self.binarysearch(nums, left, mid - 1, target)
+
+
+if __name__ == '__main__':
+    A = Solution()
+    print(A.search([1, 2], 2))
