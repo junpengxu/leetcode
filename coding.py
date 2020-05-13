7a16a9f4b18c2cece246ff5200b74ad4ac2b43df
junpengxu <weakeexu@gmai.com>
2020-05-13 23:00:58 +0800

    35. Search Insert Position

--- coding.py
+++ coding.py
@@ -1,30 +1,18 @@
+import bisect
 class Solution:
-    def search(self, nums, target: int) -> int:
-        if not nums:
-            return -1
-        left = 0
-        right = len(nums) - 1
-        self.aim = -1
-        self.binarysearch(nums, left, right, target)
-        return self.aim
-
-    def binarysearch(self, nums, left, right, target):
-
-        mid = (left + right) // 2
-        if nums[mid] == target:
-            self.aim = mid
-            return
-        elif nums[left] == target:
-            self.aim = left
-            return
-        elif nums[right] == target:
-            self.aim = right
-            return
-        if right - left > 1 and self.aim == -1:
-            self.binarysearch(nums, mid + 1, right, target)
-            self.binarysearch(nums, left, mid - 1, target)
+    def searchInsert(self, nums, target: int) -> int:
+        return bisect.bisect_left(nums, target)
 
 
 if __name__ == '__main__':
     A = Solution()
-    print(A.search([1, 2], 2))
+    assert A.searchInsert([1, 5, 5, 5, 5, 5, 5, 5, 5], 4) == 1
+    assert A.searchInsert([1, 5, 5, 5, 5, 5, 5, 5, 5], 5) == 1
+    assert A.searchInsert([1, 5, 5, 5, 5, 5, 5, 5, 5], 1) == 0
+    assert A.searchInsert([1], 4) == 1
+    assert A.searchInsert([1], 1) == 0
+    assert A.searchInsert([1], 0) == 0
+    assert A.searchInsert([1, 3], 0) == 0
+    assert A.searchInsert([1, 3], 2) == 1
+    assert A.searchInsert([1, 3], 4) == 2
+
