6c552c29a0e4c5d1c07c10f78e589c4f92275b34
junpengxu <weakeexu@gmai.com>
2020-05-23 16:51:26 +0800

    123. Best Time to Buy and Sell Stock III

--- coding.py
+++ coding.py
@@ -1,13 +1,18 @@
 class Solution:
     def maxProfit(self, prices):
-        total = 0
-        for i in range(1, len(prices)):
-            profit = prices[i] - prices[i - 1]
-            if profit > 0:
-                total += profit
-        return total
+        max_k = 2
+
+        dp_0 = [[0 for i in range(max_k)] for i in prices]
+        dp_1 = [[-prices[0] for i in range(max_k)] for i in prices]
+
+        for i in range(len(prices)):
+            for k in range(max_k):
+                if i == 0: continue
+                dp_0[i][k] = max(dp_0[i - 1][k], dp_1[i - 1][k] + prices[i])
+                dp_1[i][k] = max(dp_1[i - 1][k], dp_0[i][k - 1] - prices[i])
+        return dp_0[len(prices) - 1][max_k - 1]
 
 
 if __name__ == '__main__':
     A = Solution()
-    A.maxProfit([7, 1, 5, 3, 6, 4])
+    print(A.maxProfit([3, 3, 5, 0, 0, 3, 1, 4]))
