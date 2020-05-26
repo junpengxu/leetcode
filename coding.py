ca7598f5f41cc2875204f9ef351d92815992b91b
junpengxu <weakeexu@gmai.com>
2020-05-26 23:37:07 +0800

    40. Combination Sum II

--- coding.py
+++ coding.py
@@ -2,24 +2,22 @@ import better_exceptions
 
 
 class Solution:
-    def combinationSum(self, candidates, target):
+    def combinationSum2(self, candidates, target):
+        candidates.sort()
         res = []
 
         def dfs(candidates, index, cnt_list):
             if sum(cnt_list) > target:
                 return
             elif sum(cnt_list) < target:
-                for i in range(index, len(candidates)):
+                for i in range(index+1, len(candidates)):
                     dfs(candidates, i, cnt_list + [candidates[i]])
             else:
-                res.append(cnt_list)
-
-        dfs(candidates, 0, [])
+                if cnt_list not in res:
+                    res.append(cnt_list)
+        dfs(candidates, -1, [])
         return res
-
 
 if __name__ == '__main__':
     A = Solution()
-    print(A.combinationSum([2, 3, 5], 8))
-    print(A.combinationSum([], 8))
-    print(A.combinationSum([2,3,6,7], 7))
+    print(A.combinationSum2([10,1,2,7,6,1,5], 8))
