41e41cb8cf7e1363f38c9d1d98de7495141b78af
junpengxu <weakeexu@gmai.com>
2020-06-14 18:13:27 +0800

    41. First Missing Positive

--- coding.py
+++ coding.py
@@ -1,50 +1,17 @@
 class Solution:
-    def generateMatrix(self, n: int):
-        if not n: return
-        maxtrix = [[0 for _ in range(n)] for _ in range(n)]
-
-        def valid(node):
-            x, y = node
-            if 0 <= x and x < n and 0 <= y and y < n:
-                return True
-            return False
-
-        moves = {
-            'right': lambda node: (node[0], node[1] + 1),
-            'down': lambda node: (node[0] + 1, node[1]),
-            'left': lambda node: (node[0], node[1] - 1),
-            'up': lambda node: (node[0] - 1, node[1]),
-        }
-
-        # the way have move
-        memeory = set()
-
-        # the first init position
-        maxtrix[0][0] = 1
-        number = 2
-
-        def dfs(node):
-            memeory.add(node)
-            for _, move in moves.items():
-                # 如果下一个节点是有效的，并且没有走过
-                while valid(move(node)) and move(node) not in memeory:
-                    # 更新本节点数据
-                    memeory.add(node)
-                    node = move(node)
-                    x, y = node
-                    nonlocal number
-                    # 更新路径
-                    maxtrix[x][y] = number
-                    number+=1
-
-                if valid(node) and node not in memeory:
-                    dfs(node)
-
-        dfs((0, 0))
-        return maxtrix
+    def firstMissingPositive(self, nums):
+        nums = [item for item in nums if item >0]
+        nums.sort()
+        for index in range(len(nums)+1):
+            if index + 1 not in nums:
+                return index + 1
+        return 1
 
 
 if __name__ == '__main__':
     A = Solution()
-    print(A.generateMatrix(5))
-
+    print(A.firstMissingPositive([7, 8, 9, 11, 12]))
+    print(A.firstMissingPositive([3, 4, -1, 1]))
+    print(A.firstMissingPositive([1, 2, 3]))
+    print(A.firstMissingPositive([1, 2, 3, -1]))
+    print(A.firstMissingPositive([-1, -2, -3]))
