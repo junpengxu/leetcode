78ffc704cf54dc9475194b1e14de75fc2889168d
junpengxu <weakeexu@gmai.com>
2020-06-14 18:55:07 +0800

    48. Rotate Image

--- coding.py
+++ coding.py
@@ -1,17 +1,24 @@
 class Solution:
-    def firstMissingPositive(self, nums):
-        nums = [item for item in nums if item >0]
-        nums.sort()
-        for index in range(len(nums)+1):
-            if index + 1 not in nums:
-                return index + 1
-        return 1
+    def rotate(self, matrix):
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        dimension = len(matrix)
+        for i in range(dimension):
+            for j in range(i, dimension):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for i in range(dimension):
+            for j in range((dimension // 2)):
+                matrix[i][j], matrix[i][dimension - j - 1] = matrix[i][dimension - j - 1], matrix[i][j]
 
 
 if __name__ == '__main__':
     A = Solution()
-    print(A.firstMissingPositive([7, 8, 9, 11, 12]))
-    print(A.firstMissingPositive([3, 4, -1, 1]))
-    print(A.firstMissingPositive([1, 2, 3]))
-    print(A.firstMissingPositive([1, 2, 3, -1]))
-    print(A.firstMissingPositive([-1, -2, -3]))
+    matrix = [
+        [5, 1, 9, 11],
+        [2, 4, 8, 10],
+        [13, 3, 6, 7],
+        [15, 14, 12, 16]
+    ]
+    print(A.rotate(matrix))
