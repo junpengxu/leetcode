7eb306813a5a81fbf181c8914c2417c9c9c0a4da
junpengxu <weakeexu@gmai.com>
2020-06-21 16:36:20 +0800

    60. Permutation Sequence

--- coding.py
+++ coding.py
@@ -1,23 +1,17 @@
-from typing import List
-
-
 class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if not intervals: return []
-        intervals.sort()
-        res = [intervals[0]]
-        for item in range(1, len(intervals)):
-            if intervals[item][0] <= res[-1][1]:
-                res[-1][1] = max(res[-1][1], intervals[item][1])
-            else:
-                res.append(intervals[item])
-        return res
+    def generate_fac(self, n):
+        result = []
+        def dfs(rest, res):
+            if not rest:
+                result.append(res)
+                return
+            for item in range(len(rest)):
+                dfs(rest[:item]+rest[item+1:], res + str(rest[item]))
+
+        dfs([i for i in range(1, n + 1)], '')
+        return result
 
 
 if __name__ == '__main__':
     A = Solution()
-    print(A.merge([[1, 3], [2, 6], [8, 10], [15, 18]]))
-    print(A.merge([[1, 3], [2, 6], [0, 10], [15, 18]]))
-    print(A.merge([[1, 4], [4, 6]]))
-    print(A.merge([[1, 4], [0, 6]]))
-    print(A.merge([[1, 4], [0, 2]]))
+    print(A.generate_fac(3))
