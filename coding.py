ed452d55639cfeb9a3f5744f292376d31851459c
junpengxu <weakeexu@gmai.com>
2020-06-21 15:49:27 +0800

    56. Merge Intervals

--- coding.py
+++ coding.py
@@ -2,36 +2,22 @@ from typing import List
 
 
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        if not matrix: return []
-        directs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        memory = set()
-        matrix_x = len(matrix)
-        matrix_y = len(matrix[0])
-        move = (0, 0)
-        memory.add((0, 0))
-        result = [matrix[0][0]]
-        while True:
-            if len(memory) >= matrix_x * matrix_y:
-                return result
-            for direct in directs:
-                while True:
-                    move = (move[0] + direct[0], move[1] + direct[1])
-
-                    if not (move[0] >= 0 and move[0] < matrix_x and move[1] >= 0 and move[1] < matrix_y):
-                        move = (move[0] - direct[0], move[1] - direct[1])
-                        break
-
-                    if move not in memory:
-                        memory.add(move)
-                        result.append(matrix[move[0]][move[1]])
-                    else:
-                        move = (move[0] - direct[0], move[1] - direct[1])
-                        break
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals: return []
+        intervals.sort()
+        res = [intervals[0]]
+        for item in range(1, len(intervals)):
+            if intervals[item][0] <= res[-1][1]:
+                res[-1][1] = max(res[-1][1], intervals[item][1])
+            else:
+                res.append(intervals[item])
+        return res
 
 
 if __name__ == '__main__':
     A = Solution()
-    print(A.spiralOrder(matrix=[[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
-    print(A.spiralOrder(matrix=[]))
-    print(A.spiralOrder(matrix=[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]))
+    print(A.merge([[1, 3], [2, 6], [8, 10], [15, 18]]))
+    print(A.merge([[1, 3], [2, 6], [0, 10], [15, 18]]))
+    print(A.merge([[1, 4], [4, 6]]))
+    print(A.merge([[1, 4], [0, 6]]))
+    print(A.merge([[1, 4], [0, 2]]))
