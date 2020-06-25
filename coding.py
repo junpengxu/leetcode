f5b0bea8eabbecdd0ec22b108a349580b41160b4
junpengxu <weakeexu@gmai.com>
2020-06-26 07:08:54 +0800

    77. Combinations

--- coding.py
+++ coding.py
@@ -1,35 +1,19 @@
 class Solution:
-    def searchMatrix(self, matrix, target):
-        if not matrix: return False
+    def combine(self, n: int, k: int):
+        res = []
 
-        def binary_search(nums, target):
-            l, r = 0, len(nums) - 1
-            while l <= r:
-                mid = (l + r) // 2
-                if nums[mid] == target:
-                    return mid
-                elif nums[mid] > target:
-                    r = mid - 1
-                else:
-                    l = mid + 1
-            return False
+        def backtrace(i, k, tmp):
 
-        # find row
-        first_col = [matrix[j][0] for j in range(len(matrix))]
-        end_col = [matrix[j][-1] for j in range(len(matrix))]
-        index = zip(first_col, end_col)
-        for _index, (x, y) in enumerate(index):
-            if x <= target and target <= y:
-                break
-        nums = [matrix[_index][i] for i in range(len(matrix[0]))]
-        position = binary_search(nums, target)
-        if position is False:
-            return False
-        return True
+            if k == 0:
+                res.append(tmp)
+                return res
+            for j in range(i, n + 1):
+                backtrace(j + 1, k - 1, tmp + [j])
+
+        backtrace(1, k, [])
+        return res
 
 
 if __name__ == '__main__':
     A = Solution()
-    # print(A.searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 50]], 13))
-    # print(A.searchMatrix([[1]], 1))
-    print(A.searchMatrix([[-10,-8,-6,-4,-3],[0,2,3,4,5],[8,9,10,10,12]], 0))
+    print(A.combine(4, 2))
