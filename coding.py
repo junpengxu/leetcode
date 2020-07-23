11c6b39388cd9bb6187a0211a1fa468ff6391877
junpengxu <weakeexu@gmai.com>
2020-07-23 23:41:49 +0800

    639. Decode Ways II

--- coding.py
+++ coding.py
@@ -1,30 +1,21 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if not s:
-            return 0
-        if s[0] == "0":
-            return 0
-        size = len(s)
-        dp = [0 for _ in range(size + 1)]
-        dp[0], dp[1] = 1, 1
+        one = {'1': 1, '2': 1, '3': 1, '4': 1, '5': 1, '6': 1, '7': 1, '8': 1, '9': 1, '*': 9}
 
-        # dp[i-1] 表示选择一个（连续两个数字）
-        # dp[i] 表示选择两个（将前面的数，与自己，分别当作两个数字）
-        for i in range(1, size):
-            if s[i] == '0':
-                if s[i - 1] == '1' or s[i - 1] == '2':
-                    dp[i + 1] = dp[i - 1]
-                else:
-                    return 0
-            else:
-                if s[i-1] == '1' or (s[i-1] == '2' and "1" <= s[i] <= "6"):
-                    dp[i + 1] = dp[i - 1] + dp[i]
-                else:
-                    dp[i + 1] = dp[i]
+        two = {'10': 1, '11': 1, '12': 1, '13': 1, '14': 1, '15': 1, '16': 1, '17': 1, '18': 1, '19': 1, '20': 1,
+               '21': 1,
+               '22': 1, '23': 1, '24': 1, '25': 1, '26': 1, '*0': 2, '*1': 2, '*2': 2, '*3': 2, '*4': 2, '*5': 2,
+               '*6': 2,
+               '*7': 1, '*8': 1, '*9': 1, '1*': 9, '2*': 6, '**': 15}
 
+        dp = 1, one.get(s[:1], 0)
+
+        for i in range(1, len(s)):
+            dp = dp[1], (one.get(s[i], 0) * dp[1] + two.get(s[i - 1: i + 1], 0) * dp[0]) % 1000000007
+            print(dp)
         return dp[-1]
 
 
 if __name__ == '__main__':
     A = Solution()
-    print(A.numDecodings("1234"))
+    print(A.numDecodings("1234**12"))
