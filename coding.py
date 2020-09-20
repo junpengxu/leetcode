e1b682539b9f13eaa74e54546afaf59da68ffec5
junpengxu <weakeexu@gmai.com>
2020-09-20 23:59:52 +0800

    123. Best Time to Buy and Sell Stock III

--- coding.py
+++ coding.py
@@ -2,20 +2,25 @@ from typing import List
 
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        if not prices:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        if not prices or not k:
             return 0
-        fst_buy, fst_sell = float('-inf'), 0
-        sec_buy, sec_sell = float('-inf'), 0
-        for price in prices:
-            fst_buy = max(fst_buy, -price)
-            fst_sell = max(fst_sell, fst_buy + price)
-            sec_buy = max(fst_sell - price, sec_buy)
-            sec_sell = max(sec_sell, sec_buy + price)
 
-        return sec_sell
+        max_k = k if k<=len(prices)//2 else len(prices)//2
+
+        dp_0 = [[0 for i in range(max_k)] for i in prices]
+        dp_1 = [[float('-inf') for i in range(max_k)] for i in prices]
+
+        for i in range(len(prices)):
+            for k in range(max_k):
+                dp_0[i][k] = max(dp_0[i - 1][k], dp_1[i - 1][k] + prices[i])
+                if k == 0:
+                    dp_1[i][k] = max(dp_1[i - 1][k], - prices[i])
+                else:
+                    dp_1[i][k] = max(dp_1[i - 1][k], dp_0[i][k - 1] - prices[i])
+        return dp_0[len(prices) - 1][max_k - 1]
 
 
 if __name__ == '__main__':
     A = Solution()
-    print(A.maxProfit([7, 1, 5, 3, 6, 4]))
+    print(A.maxProfit(1, [1]))
