e2cc65a7f2849fad45cea7837118d0354c21009e
junpengxu <weakeexu@gmai.com>
2020-09-20 17:59:02 +0800

    123. Best Time to Buy and Sell Stock III

--- coding.py
+++ coding.py
@@ -3,18 +3,17 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # 定义dp 方程
-        # dp[i][0] 代表第i天手中没有股票，此时的最大利润
-        # 前一天没持有股票， 前一天持有股票（要标记为交易过）并且今天卖出
         if not prices:
             return 0
-        dp_i_0 = 0
-        dp_i_1 = float('-inf')
-        for i in range(len(prices)):
-            tmp = dp_i_0
-            dp_i_0 = max(dp_i_0, dp_i_1 + prices[i])
-            dp_i_1 = max(tmp -prices[i], dp_i_1)
-        return dp_i_0
+        fst_buy, fst_sell = float('-inf'), 0
+        sec_buy, sec_sell = float('-inf'), 0
+        for price in prices:
+            fst_buy = max(fst_buy, -price)
+            fst_sell = max(fst_sell, fst_buy + price)
+            sec_buy = max(fst_sell - price, sec_buy)
+            sec_sell = max(sec_sell, sec_buy + price)
+
+        return sec_sell
 
 
 if __name__ == '__main__':
