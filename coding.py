1e0e54977ffe9995447adb87749a228f7bfc694f
junpengxu <weakeexu@gmai.com>
2020-10-06 23:09:40 +0800

    5. 最长回文子串

--- coding.py
+++ coding.py
@@ -1,33 +1,23 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)  # 字符串的长
-        dp = [[False] * n for _ in range(n)]  # 构建一个矩阵，初始值全部为False
-        ans = ""
-        for j in range(n):  # 列
-            for i in range(j, -1, -1):  # 行
-                print(s[i:j])
-                if i == j:
-                    dp[i][j] = True
+        # dp = [[False] * n for _ in range(n)]#构建一个矩阵，初始值全部为False
+        line = [False] * n  # 只需要存储dp[i][j]前一列的值就可以计算dp[i][j]的状态
+        ans = ""  # 记录最长回文字串
+        # 枚举子串的长度 l+1
+        for j in range(0, n):
+            for i in range(0, j + 1):
+                if j == i:
+                    line[i] = True
                 elif j - i == 1:
-                    dp[i][j] = (s[i] == s[j])
+                    line[i] = (s[i] == s[j])
                 else:
-                    dp[i][j] = (dp[i + 1][j - 1] and s[i] == s[j])
-                if dp[i][j] and j - i + 1 > len(ans):
+                    line[i] = (line[i + 1] and s[i] == s[j])
+                if line[i] and j - i + 1 > len(ans):
                     ans = s[i:j + 1]
         return ans
-# 最后一次的遍历记录
-# a
-# ca
-# aca
-# kaca
-# dkaca
-# bdkaca
-# abdkaca
-# cabdkaca
-# acabdkaca
-# aacabdkaca
-# aaacabdkaca
-# aaaacabdkaca
+
+
 if __name__ == '__main__':
     A = Solution()
     # print(A.longestPalindrome("babad"))
