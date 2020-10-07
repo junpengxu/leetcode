771526307471c1103bfcbc09d914617629ff26fc
junpengxu <weakeexu@gmai.com>
2020-10-07 17:06:25 +0800

    9. 回文数

--- coding.py
+++ coding.py
@@ -1,19 +1,9 @@
 class Solution:
-    def reverse(self, x: int) -> int:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0: return False
+        num = x
         res = 0
-        flag = -1 if x < 0 else 1
-        x = x if x > 0 else -x
-        while x != 0:
-            tmp = x % 10
-            if res > (2 << 30) // 10 or (res == (2 << 30) // 10 and tmp > 7):
-                return 0
-            if res < - (2 << 30) // 10 or (res == - (2 << 30) // 10 and tmp < -8):
-                return 0
-            res = res * 10 + tmp
+        while x:
+            res = res * 10 + x % 10
             x //= 10
-        return res * flag
-
-
-if __name__ == '__main__':
-    A = Solution()
-    print(A.reverse(1534236469))
+        return res == num
