d52eb01a65182878b04f0dd87b2a3c200d505e4f
junpengxu <weakeexu@gmai.com>
2020-10-18 01:44:15 +0800

    198. 打家劫舍

--- coding.py
+++ coding.py
@@ -1,49 +1,19 @@
-class Automaton:
-    def __init__(self):
-        self.sign = 1
-        self.res = 0
-        self.state = "start"
-        self.table = {
-            "start": ["start", "sign", "is_number", "end"],
-            "sign": ["end", "end", "is_number", "end"],
-            "is_number": ["end", "end", "is_number", "end"],
-            "end": ["end", "end", "end", "end"]
-        }
-        self.INT_MAX = 2 ** 31 - 1
-        self.INT_MIN = -2 ** 31
-
-    def get_status(self, c):
-        if c == " ":
-            return 0
-        elif c == "-" or c == "+":
-            return 1
-        elif c.isdigit():
-            return 2
-        return 3
-
-    def process(self, c):
-        self.state = self.table[self.state][self.get_status(c)]
-        if self.state == "is_number":
-            self.res = int(c) + self.res * 10
-            self.res = min(self.res, self.INT_MAX) if self.sign == 1 else min(self.res, -self.INT_MIN)
-        elif self.state == "sign":
-            self.sign = 1 if c == "+" else -1
-        # 其余情况不做处理
-
-    def get_res(self):
-        return self.sign * self.res
+from typing import List
 
 
 class Solution:
-    def myAtoi(self, str: str) -> int:
-        automaton = Automaton()
-        for c in str:
-            automaton.process(c)
-        return automaton.get_res()
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        elif len(nums) <= 2:
+            return max(nums)
+        pre, pre_pre = max(nums[:2]), nums[0]
+        for index in range(2, len(nums)):
+            pre, pre_pre = max(pre, pre_pre + nums[index]), pre
+        return pre
 
 
 if __name__ == '__main__':
     A = Solution()
-    print(A.myAtoi("122w3"))
-    print(A.myAtoi("4193 with words"))
-    print(A.myAtoi("00000-42a1234"))
+    print(A.rob(nums=[2, 7, 9, 3, 1]))
+    print(A.rob(nums=[1, 2, 3, 1]))
