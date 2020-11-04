5dae5fd4bbfeafe6940aae5763ac0e70d93ab373
junpengxu <weakeexu@gmai.com>
2020-11-04 22:57:19 +0800

    14. 最长公共前缀

--- coding.py
+++ coding.py
@@ -1,19 +1,29 @@
+from typing import List
+
+
 class Solution:
-    def romanToInt(self, s: str) -> int:
-        mapping = {'M': 1000, 'CM': 900, 'D': 500, 'CD': 400, 'C': 100, 'XC': 90, 'L': 50, 'XL': 40, 'X': 10, 'IX': 9,
-                   'V': 5, 'IV': 4, 'I': 1}
-
-        def recurse(s, index, res):
-            if index >= len(s):
-                return res
-            if s[index:index + 2] in mapping:
-                return recurse(s, index + 2, res + mapping[s[index:index + 2]])
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs: return ""
+        max_prefix = strs[0]
+        for str in strs:
+            if not max_prefix:
+                return ""
             else:
-                return recurse(s, index + 1, res + mapping[s[index:index + 1]])
+                max_prefix = self.get_max_prefix(max_prefix, str)
+        return max_prefix
 
-        return recurse(s, 0, 0)
+    @staticmethod
+    def get_max_prefix(str_1, str_2):
+        res = []
+        for s_1, s_2 in zip(str_1, str_2):
+            if s_1 == s_2:
+                res.append(s_1)
+            else:
+                break
+        return "".join(res)
 
 
 if __name__ == '__main__':
     A = Solution()
-    print(A.romanToInt("III"))
+    print(A.longestCommonPrefix(["abc", "aba"]))
+    print(A.longestCommonPrefix(["cir", "car"]))
